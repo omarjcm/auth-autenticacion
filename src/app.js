08783e8180b7e48bbb90cbd08919c38009034fa5
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2023-11-20 08:13:34 -0500

    create roles.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from 'express'
 
 import user_routes from './routes/user.routes'
+import { create_roles } from './libs/initialSetup'
 
 const app = express()
+//create_roles()
 
 app.use( express.json() )
 app.use( express.urlencoded({extended:false}) )
